dc21444dfecc86fa7a838102c5a7b3038363dca7
hbernie <hbern00@gmail.com>
2023-01-11 15:46:44 -0800

    some basic set up with webpack

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  const [showButton, setShowButton] = useState(false);
+  return (
+    <div>Oh hello</div>
+  );
+}
+
+export default App;
