0cc47cd80b6c4719c94e52462d44a55de1fed9b9
hbernie <hbern00@gmail.com>
2023-04-14 13:42:00 -0700

    add project links

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -27,13 +27,13 @@ export default function Projects(){
           <p className='w-5/6'>An open source, npm middleware package that adds additional layers of security with customizable configuration for role-based authorization of GraphQL APIs.</p>
           <h3 className='text-sm mt-3 text-indigo-800'>GraphQL, React, TypeScript, Node, Express, Redis, JWT</h3>
         </div>
-        <img src={latchql} className='w-1/2 h-1/2 hover:scale-125 transition-all duration-500 rounded-md'></img>
+        <a href='https://www.npmjs.com/package/latchql' target='_blank'><img src={latchql} className='w-1/2 h-1/2 hover:scale-125 transition-all duration-500 rounded-md'></img></a>
       </div>
 
         <br></br>
 
         <div className='flex flex-row mt-3'>
-        <img src={trivia} className='w-1/2 h-1/2 hover:scale-125 transition-all duration-500 rounded-md'></img>
+        <a href='https://github.com/AlpsCo/WhatInTheWorld' target='_blank'><img src={trivia} className='w-1/2 h-1/2 hover:scale-125 transition-all duration-500 rounded-md'></img></a>
         <div className='flex flex-col justify-center ml-12'>
           <p className='text-lg font-bold'>What in the World?</p>
           <p>A geography, trivia, "edutainment" game.</p>
@@ -49,10 +49,10 @@ export default function Projects(){
           <p className='w-5/6'>A social media app to publicly or privately share and store your favorite locations.</p>
           <h3 className='text-sm mt-3 text-indigo-800'>React, PostgreSQL, Node, Express, TailwindCSS, Google Maps API</h3>
         </div>
-        <img src={spot} className='w-1/2 h-1/2 hover:scale-125 transition-all duration-500 rounded-md'></img>
+        <a href='https://github.com/MeetmeattheSpot/Meet-me-at-the-Spot/tree/dev' target='_blank'><img src={spot} className='w-1/2 h-1/2 hover:scale-125 transition-all duration-500 rounded-md'></img></a>
       </div>
 
-      <h2 className='border-2'>Some static websites: LatchQL Landing Page + Portfolio Website</h2>
+      {/* <h2 className='border-2'>Some static websites: LatchQL Landing Page + Portfolio Website</h2> */}
       </div>
       </div>
     </Element>
